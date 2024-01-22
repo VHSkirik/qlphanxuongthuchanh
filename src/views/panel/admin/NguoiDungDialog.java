f1c6a6107fc35603076007aff04980a408143735
Skirik <vh.bchieu@gmail.com>
2024-01-22 15:13:24 +0700

    create NguoiDungDialog

---
+++ src/views/panel/admin/NguoiDungDialog.java
@@ -0,0 +1,189 @@
+
+package views.panel.admin;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author @VHSkirik
+ */
+public class NguoiDungDialog extends javax.swing.JDialog {
+
+    public NguoiDungDialog(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        setUndecorated(true);
+        setBackground(new Color(255,255,255,0));
+        initComponents();
+        myInit();
+    }
+    
+    private void myInit(){
+        initImage();
+    }
+    
+    private void initImage(){
+        btThem.setIcon(new FlatSVGIcon("./views/icon/svg/Checkmark.svg",40, 40));
+        btHuy.setIcon(new FlatSVGIcon("./views/icon/svg/Cancel_2.svg",40, 40));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnMain = new views.panel.PanelBorder();
+        pnTitle = new views.panel.PanelBorder();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        btHuy = new javax.swing.JButton();
+        btThem = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        pnMain.setBackground(new java.awt.Color(255, 255, 255));
+        pnMain.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        pnTitle.setBackground(new java.awt.Color(25, 118, 211));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("THÊM TÀI KHOẢN NGƯỜI DÙNG");
+
+        javax.swing.GroupLayout pnTitleLayout = new javax.swing.GroupLayout(pnTitle);
+        pnTitle.setLayout(pnTitleLayout);
+        pnTitleLayout.setHorizontalGroup(
+            pnTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnTitleLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 360, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        pnTitleLayout.setVerticalGroup(
+            pnTitleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnTitleLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 71, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pnMain.add(pnTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        jTextField1.setFont(new java.awt.Font("JetBrains Mono Light", 0, 14)); // NOI18N
+        pnMain.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, 330, 40));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel2.setText("Tên đăng nhập");
+        pnMain.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 106, 140, 20));
+
+        jTextField2.setFont(new java.awt.Font("JetBrains Mono Light", 0, 14)); // NOI18N
+        pnMain.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 330, 40));
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel3.setText("Mật khẩu");
+        pnMain.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, 140, 20));
+
+        jTextField3.setFont(new java.awt.Font("JetBrains Mono Light", 0, 14)); // NOI18N
+        pnMain.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 270, 330, 40));
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel4.setText("Email");
+        pnMain.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, 140, 20));
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel5.setText("Loại người dùng");
+        pnMain.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 320, 140, 20));
+
+        jComboBox1.setFont(new java.awt.Font("JetBrains Mono NL Light", 0, 14)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Admin", "CanBoKiemKe", "GiaoVien" }));
+        pnMain.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 340, 330, 40));
+
+        btHuy.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btHuy.setForeground(new java.awt.Color(255, 0, 0));
+        btHuy.setText("HỦY");
+        btHuy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btHuyActionPerformed(evt);
+            }
+        });
+        pnMain.add(btHuy, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 400, 150, 40));
+
+        btThem.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btThem.setForeground(new java.awt.Color(25, 118, 211));
+        btThem.setText("SUBMIT");
+        btThem.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btThem.setIconTextGap(1);
+        pnMain.add(btThem, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 400, 150, 40));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnMain, javax.swing.GroupLayout.PREFERRED_SIZE, 471, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btHuyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btHuyActionPerformed
+        int rs = JOptionPane.showConfirmDialog(this, "Hủy bản ghi hiện tại?","Hủy",JOptionPane.YES_NO_OPTION);
+        if (rs == JOptionPane.YES_OPTION)
+            this.dispose();
+    }//GEN-LAST:event_btHuyActionPerformed
+
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException ex){
+            
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                NguoiDungDialog dialog = new NguoiDungDialog(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btHuy;
+    private javax.swing.JButton btThem;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private views.panel.PanelBorder pnMain;
+    private views.panel.PanelBorder pnTitle;
+    // End of variables declaration//GEN-END:variables
+}
