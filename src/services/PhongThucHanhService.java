82b7756d75cc348166dc58f5688ec1846d896678
doducanh2002 <anh15042002hla@gmail.com>
2024-01-26 13:01:17 +0700

    fix bug

--- src/services/PhongThucHanhService.java
+++ src/services/PhongThucHanhService.java
@@ -8,18 +8,18 @@ import models.PhongThucHanh;
 
 public class PhongThucHanhService {
 
-    public OperationResult createPhongThucHanh(String tenPhong, String loaiPhong, String diaDiem, int sucChua, String tinhTrang) {
+    public OperationResult createPhongThucHanh(String tenPhong, String loaiPhong, String diaDiem, int sucChua, String tinhTrang, String Toa) {
         // Kiểm tra trống
         if (tenPhong.isBlank() || loaiPhong.isBlank() || diaDiem.isBlank()) {
             return OperationResult.ADD_FAILURE;
         } else {
-            PhongThucHanh phongThucHanh = new PhongThucHanh(null, tenPhong, loaiPhong, diaDiem, sucChua, tinhTrang);
+            PhongThucHanh phongThucHanh = new PhongThucHanh(null, tenPhong, loaiPhong, diaDiem, sucChua, tinhTrang, Toa);
             int result = PhongThucHanhDAO.getIns().create(phongThucHanh);
             return (result == -1) ? OperationResult.ADD_FAILURE : OperationResult.ADD_SUCCESS;
         }
     }
 
-   public OperationResult updatePhongThucHanh(int maPhongThucHanh, String tenPhong, String loaiPhong, String diaDiem, int sucChua, String tinhTrang) {
+   public OperationResult updatePhongThucHanh(int maPhongThucHanh, String tenPhong, String loaiPhong, String diaDiem, int sucChua, String tinhTrang, String Toa) {
     // Kiểm tra trống
     if (tenPhong.isBlank() || loaiPhong.isBlank() || diaDiem.isBlank()) {
         return OperationResult.EDIT_FAILURE;
@@ -27,7 +27,7 @@ public class PhongThucHanhService {
         else if (PhongThucHanhDAO.getIns().findOne(maPhongThucHanh) == null) 
         return OperationResult.EDIT_FAILURE;
         else {
-        PhongThucHanh phongThucHanh = new PhongThucHanh(maPhongThucHanh, tenPhong, loaiPhong, diaDiem, sucChua, tinhTrang);
+        PhongThucHanh phongThucHanh = new PhongThucHanh(maPhongThucHanh, tenPhong, loaiPhong, diaDiem, sucChua, tinhTrang, Toa);
         int result = PhongThucHanhDAO.getIns().update(phongThucHanh, maPhongThucHanh);
         return (result == -1) ? OperationResult.EDIT_FAILURE : OperationResult.EDIT_SUCCESS;
     }
