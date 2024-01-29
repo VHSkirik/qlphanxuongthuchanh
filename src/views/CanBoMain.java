9e4c42c6924fc1ab1a2b8fc41f7e4773ca251bc7
Skirik <vh.bchieu@gmail.com>
2024-01-29 17:01:31 +0700

    update CanBoMain

--- src/views/CanBoMain.java
+++ src/views/CanBoMain.java
@@ -1,25 +1,72 @@
 package views;
 
 import java.awt.Color;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
 import models.NguoiDung;
+import views.models.CurrentUser;
+import views.panel.admin.AdminDashBroad;
+import views.panel.admin.AdminNguoiDung;
+import views.panel.admin.AdminPhong;
+import views.models.EventMenuSelected;
+import views.panel.admin.AdminThietBi;
+import views.panel.admin.AdminYeuCau;
+import views.panel.admin.FormTaiKhoan;
 
 public class CanBoMain extends javax.swing.JFrame {
     
-    private static NguoiDung user;
-
     public CanBoMain(NguoiDung user) {
-        CanBoMain.user = user;
+        CurrentUser.setNguoiDung(user);
         initComponents();
         myInit();
     }
     
     private void myInit(){
         setBackground(new Color(0, 0, 0, 0));
-        panelMenu1.initMoving(CanBoMain.this);
+        menu.initMoving(CanBoMain.this);
+        menu.addEventMenuSelected(new EventMenuSelected() {
+            @Override
+            public void selected(int index) {
+                System.out.println(index);
+                switch (index) {
+                    case 1:
+                        setMainPanel(new AdminDashBroad());
+                        break;
+                    case 2:
+                        setMainPanel(new AdminPhong());
+                        break;
+                    case 3:
+                        setMainPanel(new AdminThietBi());
+                        break;
+                    case 4:
+                        setMainPanel(new AdminNguoiDung());
+                        break;
+                    case 6:
+                        setMainPanel(new FormTaiKhoan());
+                        break;
+                    case 10:
+                        int checkLogout = JOptionPane.showConfirmDialog(CanBoMain.this, "Đăng xuất tài khoản hiện tại?", "Đăng xuất", JOptionPane.YES_NO_OPTION);
+                        if (checkLogout == JOptionPane.YES_OPTION) {
+                            CanBoMain.this.dispose();
+                            new LoginForm().setVisible(true);
+                        }
+                        break;
+                    case 13:
+                        int checkExit = JOptionPane.showConfirmDialog(CanBoMain.this, "Thoát ứng dụng?", "Thoát", JOptionPane.YES_NO_OPTION);
+                        if (checkExit == JOptionPane.YES_OPTION) {
+                            System.exit(0);
+                        }
+                        break;
+                }
+            }
+        });
     }
     
-    public static NguoiDung getUser(){
-        return CanBoMain.user;
+    private void setMainPanel(JComponent component) {
+        mainPanel.removeAll();
+        mainPanel.add(component);
+        mainPanel.repaint();
+        mainPanel.revalidate();
     }
 
     @SuppressWarnings("unchecked")
@@ -27,24 +74,43 @@ public class CanBoMain extends javax.swing.JFrame {
     private void initComponents() {
 
         panelBorder1 = new views.panel.PanelBorder();
-        panelMenu1 = new views.panel.CanBoMenu();
+        menu = new views.panel.CanBoMenu();
+        information1 = new views.panel.Information();
+        mainPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
 
         panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
 
+        information1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        mainPanel.setLayout(new java.awt.BorderLayout());
+
         javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
         panelBorder1.setLayout(panelBorder1Layout);
         panelBorder1Layout.setHorizontalGroup(
             panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelBorder1Layout.createSequentialGroup()
-                .addComponent(panelMenu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 985, Short.MAX_VALUE))
+                .addComponent(menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelBorder1Layout.createSequentialGroup()
+                        .addComponent(information1, javax.swing.GroupLayout.DEFAULT_SIZE, 963, Short.MAX_VALUE)
+                        .addGap(16, 16, 16))
+                    .addGroup(panelBorder1Layout.createSequentialGroup()
+                        .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())))
         );
         panelBorder1Layout.setVerticalGroup(
             panelBorder1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, 643, Short.MAX_VALUE)
+            .addComponent(menu, javax.swing.GroupLayout.DEFAULT_SIZE, 701, Short.MAX_VALUE)
+            .addGroup(panelBorder1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(information1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -65,7 +131,9 @@ public class CanBoMain extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private views.panel.Information information1;
+    private javax.swing.JPanel mainPanel;
+    private views.panel.CanBoMenu menu;
     private views.panel.PanelBorder panelBorder1;
-    private views.panel.CanBoMenu panelMenu1;
     // End of variables declaration//GEN-END:variables
 }
--- src/views/panel/CanBoMenu.java
+++ src/views/panel/CanBoMenu.java
@@ -10,39 +10,38 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.JFrame;
+import views.models.EventMenuSelected;
 import views.models.Model_Menu;
 
 public class CanBoMenu extends javax.swing.JPanel {
 
     private int x;
     private int y;
+    private EventMenuSelected event;
 
     public CanBoMenu() {
         initComponents();
         setOpaque(false);
-        listMenu1.setOpaque(false);
-        listMenu1.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        listMenu.setOpaque(false);
+        listMenu.setCursor(new Cursor(Cursor.HAND_CURSOR));
         myInit();
     }
 
     private void myInit() {
-        listMenu1.addItem(new Model_Menu("", "Cán Bộ Kiểm Kê", Model_Menu.MenuType.TITLE));
-        listMenu1.addItem(new Model_Menu("1", "DashBroad", Model_Menu.MenuType.MENU));
-        listMenu1.addItem(new Model_Menu("2", "Phòng", Model_Menu.MenuType.MENU));
-        listMenu1.addItem(new Model_Menu("3", "Thiết Bị", Model_Menu.MenuType.MENU));
-        listMenu1.addItem(new Model_Menu("4", "Kho", Model_Menu.MenuType.MENU));
-        listMenu1.addItem(new Model_Menu("5", "Thống Kê", Model_Menu.MenuType.MENU));
+        listMenu.addItem(new Model_Menu("", "Cán Bộ Kiểm Kê", Model_Menu.MenuType.TITLE));
+        listMenu.addItem(new Model_Menu("2", "Phòng", Model_Menu.MenuType.MENU));
+        listMenu.addItem(new Model_Menu("3", "Thiết Bị", Model_Menu.MenuType.MENU));
+        listMenu.addItem(new Model_Menu("5", "Thống Kê", Model_Menu.MenuType.MENU));
 
-        listMenu1.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
-        listMenu1.addItem(new Model_Menu("", "Cá Nhân", Model_Menu.MenuType.TITLE));
+        listMenu.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
+        listMenu.addItem(new Model_Menu("", "Cá Nhân", Model_Menu.MenuType.TITLE));
 
-        listMenu1.addItem(new Model_Menu("20", "Tài Khoản", Model_Menu.MenuType.MENU));
-        listMenu1.addItem(new Model_Menu("21", "Thông Tin", Model_Menu.MenuType.MENU));
-        listMenu1.addItem(new Model_Menu("22", "Đăng Xuất", Model_Menu.MenuType.MENU));
+        listMenu.addItem(new Model_Menu("20", "Tài Khoản", Model_Menu.MenuType.MENU));
+        listMenu.addItem(new Model_Menu("22", "Đăng Xuất", Model_Menu.MenuType.MENU));
 
-        listMenu1.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
-        listMenu1.addItem(new Model_Menu("", "Khác", Model_Menu.MenuType.TITLE));
-        listMenu1.addItem(new Model_Menu("exit", "Thoát", Model_Menu.MenuType.MENU));
+        listMenu.addItem(new Model_Menu("", "", Model_Menu.MenuType.EMPTY));
+        listMenu.addItem(new Model_Menu("", "Khác", Model_Menu.MenuType.TITLE));
+        listMenu.addItem(new Model_Menu("exit", "Thoát", Model_Menu.MenuType.MENU));
 
     }
 
@@ -64,6 +63,11 @@ public class CanBoMenu extends javax.swing.JPanel {
 
         });
 
+    }
+    
+    public void addEventMenuSelected(EventMenuSelected event){
+        this.event = event;
+        listMenu.addEventMenuSelected(event);
     }
 
     @Override
@@ -90,7 +94,7 @@ public class CanBoMenu extends javax.swing.JPanel {
         panelMoving = new javax.swing.JPanel();
         sVGImage1 = new views.label.SVGImage();
         jLabel1 = new javax.swing.JLabel();
-        listMenu1 = new views.models.ListMenu<>();
+        listMenu = new views.models.ListMenu<>();
 
         addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentShown(java.awt.event.ComponentEvent evt) {
@@ -133,14 +137,14 @@ public class CanBoMenu extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(panelMoving, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(listMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(listMenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(panelMoving, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(listMenu1, javax.swing.GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE))
+                .addComponent(listMenu, javax.swing.GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -151,7 +155,7 @@ public class CanBoMenu extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private views.models.ListMenu<String> listMenu1;
+    private views.models.ListMenu<String> listMenu;
     private javax.swing.JPanel panelMoving;
     private views.label.SVGImage sVGImage1;
     // End of variables declaration//GEN-END:variables
